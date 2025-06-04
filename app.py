b935aafd453ae200a2afb0d3da45d1a19fc6fa45
Nicole Lober <thesenpaipie@gmail.com>
2025-06-03 20:57:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
 
-import streamlit as st
-import pandas as pd
 
-st.title("Undervalued Stock Screener")
-st.caption("Based on P/E Ratio < 15")
-
-df = pd.read_csv("undervalued.csv")
-st.dataframe(df)
 
 import streamlit as st
 import pandas as pd
